71173784846373d9cf3963ddd01b15df91b58812
Diegodossantos23 <diegodossantos900@outlook.com>
2021-02-15 21:15:09 -0300

    Criei o compoente e implementei a funcao para gerar o numero aleatorio

--- gerador/src/App.js
+++ gerador/src/App.js
@@ -1,24 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import HomePage from './components/HomePage/HomePage'
+import styled from 'styled-components'
 
-function App() {
+const AppContainer = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-direction: column;
+  width: 100vw;
+  height: 100vh;
+  
+  background-color: #121214;
+`
+
+const App = () => {
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <AppContainer>
+      <HomePage/>
+    </AppContainer>
   );
 }
 
--- gerador/src/components/HomePage/HomePage.js
+++ gerador/src/components/HomePage/HomePage.js
@@ -1,10 +1,36 @@
-import React from 'react'
+import { Button } from '@material-ui/core';
+import React, { useState } from 'react';
+import { ButtonContainer, HomeContainer,ClickButton} from './StyledHomePage';
+
 
 const HomePage = () => {
-    return(
-        <div>
 
-        </div>
+    const [ numeroAleatorio, setNumeroAleatorio ] = useState(1);
+
+    const gerarNumeroAleatorio = () => {
+      const novoNumero = Math.floor(Math.random() * (100-1) + 1);
+      setNumeroAleatorio(novoNumero);
+    }
+
+    return(
+        <HomeContainer>
+            <h3>Número aleatório:</h3>
+             <h1><strong>$</strong> {numeroAleatorio}</h1>
+
+            <ButtonContainer>
+                <label>
+                Click no botão abaixo para gerar um número aleatório:
+                </label>
+
+                <ClickButton
+                variant="contained" 
+                color="primary" 
+                onClick={gerarNumeroAleatorio}>
+                    Gerar número
+                </ClickButton>
+                
+            </ButtonContainer>
+        </HomeContainer>
     )
 }
 export default HomePage
